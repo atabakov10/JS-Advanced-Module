d1a152eb2605744069e8e4bd971ba372e2e5f571
ANGEL <atabakov99@abv.bg>
2022-09-05 17:46:55 +0300

    demo file

---
+++ 01.Syntax, Functions and Statements-Lab/demo.js
@@ -0,0 +1,24 @@
+let a;
+a = 10;
+
+let b = a + 1;
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+{
+    if(a < 10){
+        console.log('a is less than 10');
+    }
+    else{
+        console.log('a is greater than or equal to 10');
+    }
+}
+function printGreeting(){
+    let c = 'hi';
+    console.log(c);
+}
+printGreeting();
+walk()
+function walk() {
+console.log('Walking..')
+}
